1d590cafcf4c622e4491c87907f4456e310211eb
Ilya Buzuev <ilya.buzuev@mail.ru>
2023-03-05 22:01:24 +0300

    added the ability to display prizes on the page history.html

--- js/prizeHistory.js
+++ js/prizeHistory.js
@@ -1,5 +1,42 @@
 const prizeHistory = () => {
+  const historyList = document.getElementById('historyList');
+  const clearPrizeHistoryButton = document.getElementById('clearPrizeHistoryButton');
 
+  function getPrizeHistory() {
+    return JSON.parse(localStorage.getItem('prizeHistory'));
+  }
+
+  function setHistoryContent(prizeHistory) {
+    if (prizeHistory) {
+      prizeHistory.forEach((prize, index) => {
+        historyList.innerHTML += `
+          <li class="history__item item-history" data-id="${index}">
+            <p class="item-history__name ${prize[3]}">${prize[0]}</p>
+            <img class="item-history__img" src=${prize[2]} alt=${prize[0]}>
+            <p class="item-history__description">${prize[5]}</p>
+          </li>
+        `
+      });
+    } else {
+      historyList.innerHTML = `<p class="history__message">История выпадений отсутсвует</p>`;
+    }
+  }
+
+  function clearPrizeHistory() {
+    historyList.innerHTML = '';
+
+    localStorage.removeItem('prizeHistory');
+
+    historyList.innerHTML = `<p class="history__message">История выпадений отсутсвует</p>`;
+  }
+
+  clearPrizeHistoryButton.addEventListener('click', () => {
+    clearPrizeHistory();
+  });
+
+  let prizeHistory = getPrizeHistory();
+
+  setHistoryContent(prizeHistory);
 };
 
 prizeHistory();
